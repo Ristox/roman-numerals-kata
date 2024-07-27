119b04cdc5f483ae8af53da08b0a79ea8ba2c397
Risto Uibo <risto@rsx.ee>
2024-07-27 22:48:12 +0300

    initial commit

---
+++ src/main/kotlin/ee/rsx/kata/codurance/romannumerals/RomanNumber.kt
@@ -0,0 +1,4 @@
+package ee.rsx.kata.codurance.romannumerals
+
+class RomanNumber() {
+}
---
+++ src/test/kotlin/ee/rsx/kata/codurance/romannumerals/RomanNumberTest.kt
@@ -0,0 +1,16 @@
+package ee.rsx.kata.codurance.romannumerals
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+/**
+  https://www.codurance.com/katas/shopping-cart-kata
+ **/
+
+class RomanNumberTest {
+
+  @Test
+  fun `test`() {
+  }
+
+}

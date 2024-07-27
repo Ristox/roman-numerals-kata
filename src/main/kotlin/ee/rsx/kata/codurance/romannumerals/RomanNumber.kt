f8d39aea04cd60027491379ec05dff831198847e
Risto Uibo <risto@rsx.ee>
2024-07-27 23:05:45 +0300

    test for 2 -> II (green)

--- src/main/kotlin/ee/rsx/kata/codurance/romannumerals/RomanNumber.kt
+++ src/main/kotlin/ee/rsx/kata/codurance/romannumerals/RomanNumber.kt
@@ -1,7 +1,7 @@
 package ee.rsx.kata.codurance.romannumerals
 
-class RomanNumber(value: Int) {
+class RomanNumber(private val value: Int) {
   override fun toString(): String {
-    return "I"
+    return if (value == 1) "I" else "II"
   }
 }

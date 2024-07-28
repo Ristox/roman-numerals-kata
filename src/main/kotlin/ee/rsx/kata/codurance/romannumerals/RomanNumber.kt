a2e1947347257288f378818876cdee3072e260bf
Risto Uibo <risto@rsx.ee>
2024-07-28 16:05:10 +0300

    refactor to symbols map and value ranges: 1..9 -> I..IX (refactor)

--- src/main/kotlin/ee/rsx/kata/codurance/romannumerals/RomanNumber.kt
+++ src/main/kotlin/ee/rsx/kata/codurance/romannumerals/RomanNumber.kt
@@ -1,18 +1,24 @@
 package ee.rsx.kata.codurance.romannumerals
 
 class RomanNumber(private val value: Int) {
+
+  private val arabicToRomanNumber = linkedMapOf(
+    1 to "I",
+    5 to "V",
+    10 to "X"
+  )
+
   override fun toString(): String {
-    return when (value) {
-      1 -> "I"
-      2 -> "II"
-      3 -> "III"
-      4 -> "IV"
-      5 -> "V"
-      6 -> "VI"
-      7 -> "VII"
-      8 -> "VIII"
-      9 -> "IX"
+
+    return when {
+      value < 4 -> 1.asRoman().repeat(value)
+      value == 4 -> 1.asRoman() + 5.asRoman()
+      value == 5 -> 5.asRoman()
+      value < 9 -> 5.asRoman() + 1.asRoman().repeat(value - 5)
+      value == 9 -> 1.asRoman() + 10.asRoman()
       else -> ""
     }
   }
+
+  private fun Int.asRoman() = arabicToRomanNumber.getValue(this)
 }

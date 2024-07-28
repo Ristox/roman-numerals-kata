81e3f7f1ae535dd508688e6b213536f177b0f304
Risto Uibo <risto@rsx.ee>
2024-07-28 15:50:01 +0300

    refactor tests to cover range 1..9 -> I..IX (red)

--- src/test/kotlin/ee/rsx/kata/codurance/romannumerals/RomanNumberTest.kt
+++ src/test/kotlin/ee/rsx/kata/codurance/romannumerals/RomanNumberTest.kt
@@ -2,6 +2,10 @@ package ee.rsx.kata.codurance.romannumerals
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 /**
   https://www.codurance.com/katas/roman-numerals
@@ -9,18 +13,28 @@ import org.junit.jupiter.api.Test
 
 class RomanNumberTest {
 
-  @Test
-  fun `number 1 is roman numeral 'I'`() {
-    1.assertIsRomanNumber("I")
-  }
-
-  @Test
-  fun `number 2 is roman numeral 'II'`() {
-    2.assertIsRomanNumber("II")
+  @ParameterizedTest
+  @MethodSource("singleValues")
+  fun `number $number is roman numeral $roman` (number: Int, roman: String) {
+    number.assertIsRomanNumber(roman)
   }
 
   private fun Int.assertIsRomanNumber(roman: String) {
     assertThat("${RomanNumber(this)}").isEqualTo(roman)
   }
 
+  companion object {
+    @JvmStatic
+    fun singleValues(): Stream<Arguments> = Stream.of(
+      Arguments.of(1, "I"),
+      Arguments.of(2, "II"),
+      Arguments.of(3, "III"),
+      Arguments.of(4, "IV"),
+      Arguments.of(5, "V"),
+      Arguments.of(6, "VI"),
+      Arguments.of(7, "VII"),
+      Arguments.of(8, "VIII"),
+      Arguments.of(9, "IX"),
+    )
+  }
 }

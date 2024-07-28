74c998f275f7e5890b3edfa8b8e307e47f72ccd8
Risto Uibo <risto@rsx.ee>
2024-07-28 19:46:45 +0300

    test for 10..19 -> X..XIX (green)

--- src/main/kotlin/ee/rsx/kata/codurance/romannumerals/RomanNumber.kt
+++ src/main/kotlin/ee/rsx/kata/codurance/romannumerals/RomanNumber.kt
@@ -21,6 +21,11 @@ class RomanNumber(private val value: Int) {
       value == 5 -> 5.asRoman()
       value < 9 -> 5.asRoman() + 1.asRoman().repeat(value - 5)
       value == 9 -> 1.asRoman() + 10.asRoman()
+
+      value in 10..13 -> 10.asRoman() + 1.asRoman().repeat(value - 10)
+      value <= 15 -> 10.asRoman() + 1.asRoman().repeat(15 - value) +  5.asRoman()
+      value < 19 -> 10.asRoman() + 5.asRoman() + 1.asRoman().repeat(value - 15)
+      value == 19 -> 10.asRoman() + 1.asRoman() + 10.asRoman()
       else -> ""
     }
   }
